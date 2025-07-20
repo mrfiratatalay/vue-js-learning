77a2229f2cb4141e8c0c90f89003066892784da8
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-20 15:45:01 +0300

    starting to vue

---
+++ 003/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Composition API</h1>
+    <IconVue name="vite" size="large" />
+  </div>
+</template>
+
+<script setup>
+import IconVue from './components/IconVue.vue';
+</script>
---
+++ 003/src/components/IconVue.vue
@@ -0,0 +1,31 @@
+<template>
+    <img :src="iconPath" :class="sizeClass" />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    size: String,
+    name: String
+})
+
+const iconPath = computed(() => `/public/${props.name}.svg`)
+const sizeClass = computed(()=>`${props.size}-icon`)
+</script>
+
+
+<style scoped>
+.small-icon {
+  width: 16px;
+  height: 16px;
+}
+.medium-icon {
+  width: 32px;
+  height: 32px;
+}
+.large-icon {
+  width: 48px;
+  height: 48px;
+}
+</style>
