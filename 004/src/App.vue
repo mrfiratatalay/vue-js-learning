256a9b8f72b0fa814a0999cb3e9a7a38c031e49c
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-20 16:51:14 +0300

    starting to vue

---
+++ 004/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <HelloWorld />
+    <SocialPost />
+  </div>
+</template>
+
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import SocialPost from './components/molecules/SocialPost.vue';
+</script>
---
+++ 004/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Bu benim ilk componentim</h1>
+
+  </div>
+</template>
+
+
+<script setup>
+</script>
+
+
+<style></style>
