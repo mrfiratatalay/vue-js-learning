fd6128c726dd372ed61b60b57d1a650afc0b66de
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-20 15:32:44 +0300

    starting to vue

---
+++ 002/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Options API </h2>
+    <IconVue name="vue" size="small"/>
+  </div>
+</template>
+
+
+<script setup>
+import IconVue from './components/atoms/IconVue.vue';
+</script>
+<style></style>
---
+++ 002/src/components/atoms/IconVue.vue
@@ -0,0 +1,43 @@
+<template>
+    <img :src="iconPath" :class="sizeClass"/>
+</template>
+
+
+
+
+<script>
+export default {
+    name: 'IconVue',
+    props: {
+        size: String,
+        name: String
+    },
+    computed: {
+        iconPath() {
+            return `/public/${this.name}.svg`;
+        },
+        sizeClass() {
+            return `${this.size}-icon`;
+        }
+
+
+    }
+}
+
+</script>
+
+
+<style scoped>
+.small-icon {
+  width: 16px;
+  height: 16px;
+}
+.medium-icon {
+  width: 32px;
+  height: 32px;
+}
+.large-icon {
+  width: 48px;
+  height: 48px;
+}
+</style>
