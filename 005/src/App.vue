6dca50e9238b17fd812f9c0b002d51464bd2cb60
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-21 14:14:45 +0300

    This is my first component with props

---
+++ 005/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>This is my first component with props</h1>
+    <SocialPost
+      username="FIRAT ATALAY  👨‍💻"
+      userId="firatatalay"
+      avatarSrc="https://i.pravatar.cc/40?u=1"
+      post="This is my first post with props in Vue.js"
+    />
+  </div>
+</template>
+
+<script setup>
+import SocialPost from './components/molecules/SocialPost.vue';
+</script>
