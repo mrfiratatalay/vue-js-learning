7b15585e641238a22dc19c55e3792f223d30e168
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-22 16:27:07 +0300

    Vue methods and computed() property

---
+++ 008/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <TheHeader />
+    <TheWelcome />
+</template>
+
+<script setup>
+import TheHeader from './components/organisms/TheHeader.vue';
+import TheWelcome from './components/TheWelcome.vue';
+</script>
+
+<style scoped>
+</style>
---
+++ 008/src/components/organisms/TheHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <div v-html="logo"></div>
+        <h1>Companion App</h1>
+        <a href="#">
+            <span v-text="fullName"></span>
+        </a>
+    </header>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+
+const name = ref("Firat");
+const surname = ref("Atalay");
+const fullName = computed(() => {
+  return `${name.value} ${surname.value}`;
+});
+
+
+const logo = `<svg
+  height="50"
+  width="50"
+  viewBox="0 0 210 210"
+>
+  <polygon
+  	points="100,10 40,198 190,78 10,78 160,198"
+    style="fill:grey;"/>
+</svg>`;
+</script>
+
+
+<style lang="scss">
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    grid-column-start: 1;
+    grid-column-end: 3;
+    margin-bottom:24px;
+
+    a span {
+        font-weight: bold;
+    }
+}
+
+</style>
