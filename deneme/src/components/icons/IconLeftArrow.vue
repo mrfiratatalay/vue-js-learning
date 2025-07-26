cf901fa037735ba8aae20146d82c37592136c216
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-26 23:37:16 +0300

    I try to repeat last week what i learnt

--- deneme/src/components/icons/IconLeftArrow.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
-    <path d="M15 7L10 12L15 17" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-</template>
