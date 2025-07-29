fa83966333209c3e9a8552b153fba2b5d9789ee1
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-29 14:47:19 +0300

    Testing

---
+++ 011/src/components/__tests__/TheButton.spec.js
@@ -0,0 +1,11 @@
+import { expect, describe, it } from "vitest";
+import { mount } from "@vue/test-utils";
+import component from "../atoms/TheButton.vue"
+
+describe("TheButton.vue", () => {
+    describe("when mounted", () => {
+        it("renders properly", () => {
+            //Test buraya gelecek
+        })
+    })
+})
