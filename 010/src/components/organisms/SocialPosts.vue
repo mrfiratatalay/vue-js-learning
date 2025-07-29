b6fb9d7cd99d0baa852cd35020b07c149c5f9816
Fırat Atalay <firatatalay.cs@gmail.com>
2025-07-29 09:36:05 +0300

    watch

--- 010/src/components/organisms/SocialPosts.vue
+++ 010/src/components/organisms/SocialPosts.vue
@@ -12,19 +12,30 @@
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref, watch } from "vue";
 import SocialPost from '../molecules/SocialPost.vue';
+
+const page = ref(0);
+const posts = reactive([]);
+
+watch(
+  posts,
+  (newValue, oldValue) => {
+    if (newValue.length < 4) {
+      page.value++;
+      fetchPosts(page.value);
+    }
+  }
+);
 
 const onDelete = (postIndex) => {
     posts.splice(postIndex, 1);
 }
 
 
-const posts = reactive([]);
-
-const fetchPosts = () => {
+const fetchPosts = (page) => {
   const baseUrl = "https://dummyapi.io/data/v1";
-  fetch(`${baseUrl}/post?limit=5`, {
+  fetch(`${baseUrl}/post?limit=5&page=${page}`, {
     headers: {
       "app-id": "657a3106698992f50c0a5885"
     }
@@ -35,6 +46,6 @@ const fetchPosts = () => {
     });
 }
 
-fetchPosts();
+fetchPosts(page.value);
 
 </script>
