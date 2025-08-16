f053adbbba6a9561069917bf02dad5d61c0322a4
Fırat Atalay <firatatalay.cs@gmail.com>
2025-08-16 16:28:41 +0300

    vue-pinia

---
+++ 015/src/components/organisms/SideBar.vue
@@ -0,0 +1,72 @@
+<template>
+    <aside :class="{ 'sidebar__closed': closed}">
+        <template v-if="closed">
+            <IconRightArrow class="sidebar__icon" @click="toggleSidebar" />
+        </template>
+        <template v-else>
+            <h2>Sidebar</h2>
+            <IconLeftArrow class="sidebar__icon" @click="toggleSidebar" />
+            <TheButton>Create post</TheButton>
+            <div>
+                Current time: {{currentTime}}
+            </div>
+            <TheButton @click="onUpdateTimeClick">Update Time</TheButton>
+
+            <router-link to="privacy">Privacy</router-link>
+            <router-link to="about">About</router-link>
+
+            <a @click="navigateToPrivacy">Programmatic to privacy</a>
+        </template>
+    </aside>
+</template>
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+import TheButton from '../atoms/TheButton.vue';
+import IconLeftArrow from '../icons/IconLeftArrow.vue';
+import IconRightArrow from '../icons/IconRightArrow.vue';
+const currentTime = ref(new Date().toLocaleTimeString());
+const closed = ref(false);
+const toggleSidebar = () => {
+    closed.value = !closed.value;
+    window.localStorage.setItem("sidebar", closed.value);
+}
+const onUpdateTimeClick = () => {
+    currentTime.value = new Date().toLocaleTimeString();
+};
+
+onBeforeMount( async () => {
+    const sidebarState = window.localStorage.getItem("sidebar");
+    closed.value = sidebarState === "true";
+});
+
+
+
+const router = useRouter()
+const navigateToPrivacy = (event) => {
+    event.preventDefault();
+    console.log("Run a side effect");
+    router.push("privacy");
+}
+
+
+</script>
+<style scoped lang="scss">
+aside {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+
+    &.sidebar__closed{
+        width: 40px;
+    }
+
+}
+
+.sidebar__icon{
+        position: absolute;
+        right: 12px;
+        top: 22px;
+        cursor: pointer;
+    }
+</style>
