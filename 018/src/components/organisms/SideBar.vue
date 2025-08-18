34337e71fe293c14566cd2df11cb52e79f5e6854
Fırat Atalay <firatatalay.cs@gmail.com>
2025-08-18 23:27:53 +0300

    vee-validate

---
+++ 018/src/components/organisms/SideBar.vue
@@ -0,0 +1,71 @@
+<template>
+    <aside :class="{ 'sidebar__closed': sidebarStore.closed}">
+        <template v-if="sidebarStore.closed">
+            <IconRightArrow class="sidebar__icon" @click="sidebarStore.toggleSidebar" />
+        </template>
+        <template v-else>
+            <h2>Sidebar</h2>
+            <IconLeftArrow class="sidebar__icon" @click="sidebarStore.toggleSidebar" />
+            <TheButton>Create post</TheButton>
+            <div>
+                Current time: {{currentTime}}
+            </div>
+            <TheButton @click="onUpdateTimeClick">Update Time</TheButton>
+
+            <router-link to="privacy">Privacy</router-link>
+            <router-link to="about">About</router-link>
+
+            <a @click="navigateToPrivacy">Programmatic to privacy</a>
+        </template>
+    </aside>
+</template>
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+import { useSidebarStore } from '../../stores/sidebar';
+import TheButton from '../atoms/TheButton.vue';
+import IconLeftArrow from '../icons/IconLeftArrow.vue';
+import IconRightArrow from '../icons/IconRightArrow.vue';
+
+const currentTime = ref(new Date().toLocaleTimeString());
+
+const sidebarStore = useSidebarStore();
+
+const onUpdateTimeClick = () => {
+    currentTime.value = new Date().toLocaleTimeString();
+};
+
+onBeforeMount(() => {
+    sidebarStore.loadSidebarFromLocalStorage();
+});
+
+
+
+const router = useRouter()
+const navigateToPrivacy = (event) => {
+    event.preventDefault();
+    console.log("Run a side effect");
+    router.push("privacy");
+}
+
+
+</script>
+<style scoped lang="scss">
+aside {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+
+    &.sidebar__closed{
+        width: 40px;
+    }
+
+}
+
+.sidebar__icon{
+        position: absolute;
+        right: 12px;
+        top: 22px;
+        cursor: pointer;
+    }
+</style>
